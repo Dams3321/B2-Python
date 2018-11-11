652307c1e209a0c930d2658f3746d3418c55559d
Dams3321 <damien.pommier@ynov.com>
2018-11-11 22:05:07 +0100

    Create 1b-dic.py

---
+++ 1b-dic.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+# description: saisie utilisateur de plusieurs prénoms.
+# date: 15/10/2018
+# auteur : Damien Pommier
+
+Tab =()
+
+While prenoms != 'q' : 
+Prenom = input (taper le prenom)
+Tab.append (prenom)
+Del.Tab - 
+Tab sort ()
+Print(tab)
+	Print ("Taper Q pour arrêter")
